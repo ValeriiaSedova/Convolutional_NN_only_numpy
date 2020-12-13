9069ed49596238afa77783ffdd34bcd28609968e
dobrik4ever <dobrik4ever@gmail.com>
2020-12-13 18:41:38 +0100

    conv 50proc

--- src_to_implement/Layers/Conv.py
+++ src_to_implement/Layers/Conv.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import signal
 
 class Conv:
 
@@ -6,13 +7,31 @@ class Conv:
         self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
         self.num_kernels = num_kernels
-        self.weights = np.random.random(num_kernels, self.convolution_shape)
+        self.weights = np.random.random([num_kernels, *self.convolution_shape])
         self.bias = np.random.random(num_kernels)
-
+    
     def forward(self, input_tensor):
-        for element in range(input_tensor.shape[0]):
-            for kernel in range(self.num_kernels):
-                pass
+        batch, channels, y, x = input_tensor.shape
+        output_tensor = np.zeros([batch, self.num_kernels, y, x])
+        # if type(self.stride_shape) == tuple:
+        #     cc, cy, cx = self.convolution_shape
+        for element in range(batch):
+            for k in range(self.num_kernels):
+                kernel = self.weights[k]
+                loc_maps = np.zeros(channels, y, x)
+
+                for channel in range(channels):
+                    local = input_tensor[element, channel]
+                    loc_maps[channel] = signal.convolve2d(kernel[channel], local, mode = 'same')
+                
+                feature_map = loc_maps.sum(axis = 0) + self.bias[channel]
+                output_tensor[element, k] = feature_map
+        
+        return output_tensor
+
+                    
+
+
 
 
         
